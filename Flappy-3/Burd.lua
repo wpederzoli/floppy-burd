8233c36dca4c80ee74e8600e4e3754180759eaec
William <wpederzoli@gmail.com>
2018-09-10 17:44:08 +0200

    Gravity added to Burd class

---
+++ Flappy-3/Burd.lua
@@ -0,0 +1,25 @@
+Burd = Class{}
+
+local GRAVITY = 20
+
+function Burd:init()
+    self.image = love.graphics.newImage('burd.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    self.dy = 0
+end
+
+function Burd:update(dt)
+   self.dy = self.dy + GRAVITY * dt 
+
+   self.y = self.y + self.dy
+end
+
+function Burd:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
+
