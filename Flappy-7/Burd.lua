2b2530c4304ebfd8a5c0955b14bc518580307591
William <wpederzoli@gmail.com>
2018-09-11 01:25:02 +0200

    Image changed and collision bug fixed

--- Flappy-7/Burd.lua
+++ Flappy-7/Burd.lua
@@ -14,7 +14,7 @@ function Burd:init()
 end
 
 function Burd:collides(pipe)
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+    if (self.x + 2) + (self.width - 8) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
         if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
             return true
         end
