38206d1af96bb31d4cc0d47531ac55d9a88be7c9
William <wpederzoli@gmail.com>
2018-09-12 02:58:04 +0200

    Countdown added and bugs fixed

---
+++ Flappy-10/Burd.lua
@@ -0,0 +1,38 @@
+Burd = Class {}
+
+local GRAVITY = 20
+
+function Burd:init()
+    self.image = love.graphics.newImage("burd.png")
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    self.dy = 0
+end
+
+function Burd:collides(pipe)
+    if (self.x + 2) + (self.width - 8) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+    end
+
+    return false
+end
+
+function Burd:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+
+    if love.keyboard.wasPressed("space") then
+        self.dy = -5
+    end
+
+    self.y = self.y + self.dy
+end
+
+function Burd:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
